b4265fa59dc5138d66375d2c9c91c4f4e0ebd298
이준규 <jkjkjk5710@gmail.com>
2022-02-06 18:12:31 +0900

    토탈 로직 구현완료

--- app.js
+++ app.js
@@ -1,13 +1,74 @@
 const express = require('express')
 const got = require('got') // 버전 낮춰야댐 이유 알아보기 ;
 const app = express()
+const async = require('async')
+const { json } = require('express/lib/response')
+// const { response } = require('express')
 
 
 var to = 0
+
+async.waterfall([
+    function(callback) { //태그 다 가져오기
+        got.post("https://v2.velog.io/graphql", {
+            headers: {
+                cookie: "_ga=GA1.2.402592865.1574765075; __gads=ID=1d61987ced4df306-226a319d7cc600d5:T=1616220310:RT=1616220310:S=ALNI_Ma3n58Jmjk2d9IittCQnnfAJw0bIw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; _gid=GA1.2.852154745.1644056278; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; _gat_gtag_UA_125599395_1=1    }"            },
+            json: {
+                "operationName":"Posts",
+                "variables":{
+                    "username":"isntkyu",
+                    "tag":null
+                },
+                "query":"query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $limit: Int) {\n  posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $limit) {\n    id\n    title\n    short_description\n    thumbnail\n    user {\n      id\n      username\n      profile {\n        id\n        thumbnail\n        __typename\n      }\n      __typename\n    }\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    is_private\n    likes\n    __typename\n  }\n}\n"
+            }
+        }).then(response => {
+            let posts = JSON.parse(response.body).data.posts
+            let postIds = posts.map(post => post.id)
+            // console.log(postIds)
+            callback(null, postIds);
+        })
+    },
+    function(arg1, callback) { // 태그이름 담기
+        let postsLength = arg1.length
+        console.log("post s Length: ", postsLength)
+
+        let count = 0;
+        let total = 0;
+        async.whilst(
+            function test(cb) { cb(null, count < postsLength); },
+            function iter(callback) {
+                got.post("https://v2.velog.io/graphql", {
+                    headers: {
+                        cookie: "_ga=GA1.2.402592865.1574765075; __gads=ID=1d61987ced4df306-226a319d7cc600d5:T=1616220310:RT=1616220310:S=ALNI_Ma3n58Jmjk2d9IittCQnnfAJw0bIw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; _gid=GA1.2.852154745.1644056278; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; _gat_gtag_UA_125599395_1=1    }"                },
+                    json: {
+                        "operationName":"GetStats",
+                        "variables":{
+                            "post_id":`${arg1[count]}`
+                        },
+                        "query":"query GetStats($post_id: ID!) {\n  getStats(post_id: $post_id) {\n    total\n    count_by_day {\n      count\n      day\n      __typename\n    }\n    __typename\n  }\n}\n"
+                    }
+                }).then(response => {
+                    count++;
+                    let result = JSON.parse(response.body)
+                    let view = result.data.getStats.total
+                    total += view
+                    // console.log(to)
+                    callback();
+                })
+            },
+            function (err, n) {
+                console.log(total)
+                callback(null, total);
+            }
+        );
+    },
+], function (err, result) {
+    console.log(result)
+});
+
 got.post("https://v2.velog.io/graphql", {
     headers: {
-        cookie: "access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MDU2Mjc4LCJleHAiOjE2NDQwNTk4NzgsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.IE50xl4z-s9dhUbyU35hbBbsrDQG3yW2QoYcrYP60mE;"
-    },
+        cookie: "_ga=GA1.2.402592865.1574765075; __gads=ID=1d61987ced4df306-226a319d7cc600d5:T=1616220310:RT=1616220310:S=ALNI_Ma3n58Jmjk2d9IittCQnnfAJw0bIw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; _gid=GA1.2.852154745.1644056278; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; _gat_gtag_UA_125599395_1=1    }" },
     json: {"operationName":"UserTags","variables":{"username":"isntkyu"},"query":"query UserTags($username: String) {\n  userTags(username: $username) {\n    tags {\n      id\n      name\n      description\n      posts_count\n      thumbnail\n      __typename\n    }\n    posts_count\n    __typename\n  }\n}\n"}
     }).then(response => {
         let data = JSON.parse(response.body)
@@ -16,28 +77,25 @@ got.post("https://v2.velog.io/graphql", {
 
         got.post("https://v2.velog.io/graphql", {
             headers: {
-                cookie: "access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MDU2Mjc4LCJleHAiOjE2NDQwNTk4NzgsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.IE50xl4z-s9dhUbyU35hbBbsrDQG3yW2QoYcrYP60mE;"
-            },
+                cookie: "_ga=GA1.2.402592865.1574765075; __gads=ID=1d61987ced4df306-226a319d7cc600d5:T=1616220310:RT=1616220310:S=ALNI_Ma3n58Jmjk2d9IittCQnnfAJw0bIw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; _gid=GA1.2.852154745.1644056278; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; _gat_gtag_UA_125599395_1=1    }"            },
             json: {"operationName":"Posts","variables":{"username":"isntkyu","tag":`${tags}`},"query":"query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $limit: Int) {\n  posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $limit) {\n    id\n    title\n    short_description\n    thumbnail\n    user {\n      id\n      username\n      profile {\n        id\n        thumbnail\n        __typename\n      }\n      __typename\n    }\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    is_private\n    likes\n    __typename\n  }\n}\n"}
         }).then(resp => {
             // console.log(resp.body)
             let postId = JSON.parse(resp.body)
-            console.log(postId.data.posts[0].id)
+            // console.log(postId.data.posts[0].id)
 
             got.post("https://v2.velog.io/graphql", {
                 headers: {
-                    cookie: "access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MDU5MDIzLCJleHAiOjE2NDQwNjI2MjMsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.9gBqDNwc8splF7CfeGJ2Yk1BnoYwUTD9m9tIV9BVmww;"
-                },
+                    cookie: "_ga=GA1.2.402592865.1574765075; __gads=ID=1d61987ced4df306-226a319d7cc600d5:T=1616220310:RT=1616220310:S=ALNI_Ma3n58Jmjk2d9IittCQnnfAJw0bIw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwidG9rZW5faWQiOiI5MjdjMjBjZS05YjQyLTRmZGItOGRmYi1lNWY5NWQ3YTIyYmQiLCJpYXQiOjE2NDM1Mzc4NjEsImV4cCI6MTY0NjEyOTg2MSwiaXNzIjoidmVsb2cuaW8iLCJzdWIiOiJyZWZyZXNoX3Rva2VuIn0.r7uaM9_UMEh9SGA1_2umCWli9IkSByJSGOGCAqoaeAw; _gid=GA1.2.852154745.1644056278; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzJlNTgwYzMtYzE4OC00NzBiLWFiYzAtMTg1NThjZDRlZGRkIiwiaWF0IjoxNjQ0MTM1NDc2LCJleHAiOjE2NDQxMzkwNzYsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.YpJyylscaK9S6BbVRR8WiOCcyavAI7f0tMssktKZO_U; _gat_gtag_UA_125599395_1=1    }"                },
                 json: {"operationName":"GetStats","variables":{"post_id":`${postId.data.posts[0].id}`},"query":"query GetStats($post_id: ID!) {\n  getStats(post_id: $post_id) {\n    total\n    count_by_day {\n      count\n      day\n      __typename\n    }\n    __typename\n  }\n}\n"}
             }).then(respon => {
-                console.log(respon.body)
+                // console.log(respon.body)
                 let result = JSON.parse(respon.body)
-                console.log(result.data.getStats.total)
 
                 let view = result.data.getStats.total
                 to += view
 
-                console.log(to)
+                // console.log(to)
             })
             
             // res.send({profile: data.data.userTags.posts_count})
