e540d7a700ff47a57dac91751e7756dc0cfa7ae8
isntkyu <jkjkjk5710@gmail.com>
2022-02-15 19:56:12 +0900

    주석제거

--- app.js
+++ app.js
@@ -140,6 +140,7 @@ app.post('/search_post', function (req, res) {
                     "query":"query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $limit: Int) {\n  posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $limit) {\n    id\n    title\n    short_description\n    thumbnail\n    user {\n      id\n      username\n      profile {\n        id\n        thumbnail\n        __typename\n      }\n      __typename\n    }\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    is_private\n    likes\n    __typename\n  }\n}\n"
                 }
             }).then(response => {
+                console.log(JSON.parse(response.body).data.posts)
                 let posts = JSON.parse(response.body).data.posts
                 let postIds = posts.map(post => post.id)
                 // console.log(postIds)
